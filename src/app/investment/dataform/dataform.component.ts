17b203d41beb7ba7c3c6775c4f0bb1802e878933
mohamed857 <mohamed857>
2024-07-13 00:43:12 +0300

    first commit

--- src/app/investment/dataform/dataform.component.ts
+++ src/app/investment/dataform/dataform.component.ts
@@ -18,32 +18,5 @@ export class DataformComponent {
     this.data.emit(this.investmentData);
 
   }
-  calculateInvestimentResults(investment: InvestmentData): void {
-    const annualData: InvestmentResult[] = [];
-    let investmentValue = investment.amount;
-
-    for (let i = 0; i < investment.duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear =
-        investmentValue * (investment.returnRate / 100);
-      investmentValue += interestEarnedInYear + investment.annualInvestment;
-      const totalInterest =
-        investmentValue -
-        investment.annualInvestment * year -
-        investment.amount;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: investment.annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested:
-          investment.amount + investment.annualInvestment * year,
-      });
-    }
-
-    // return annualData;
-    this.data.emit(this.investmentData);
-
-  }
+  
 }
--- src/app/investment/investment.component.ts
+++ src/app/investment/investment.component.ts
@@ -1,7 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnChanges, signal, SimpleChanges } from '@angular/core';
 import { DataformComponent } from "./dataform/dataform.component";
 import { CurrencyPipe } from '@angular/common';
-import { InvestmentData } from './investmentData';
+import { InvestmentData, InvestmentResult } from './investmentData';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -11,13 +11,48 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './investment.component.html',
   styleUrl: './investment.component.css'
 })
-export class InvestmentComponent {
+export class InvestmentComponent implements OnChanges{
+  ngOnChanges(changes: SimpleChanges): void {
+    this.calculateInvestimentResults(this.investmentData);
+  }
   investmentData!:InvestmentData;
+  data!:InvestmentResult[];
   duration=signal(0);
   calculateInvestment(data:InvestmentData) {
     this.investmentData = data;
     this.duration.set(data.duration);
   }
+
+  calculateInvestimentResults(investment: InvestmentData): void {
+    const annualData: InvestmentResult[] = [];
+    let investmentValue = investment.amount;
+
+    for (let i = 0; i < investment.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear =
+        investmentValue * (investment.returnRate / 100);
+      investmentValue += interestEarnedInYear + investment.annualInvestment;
+      const totalInterest =
+        investmentValue -
+        investment.annualInvestment * year -
+        investment.amount;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: investment.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested:
+          investment.amount + investment.annualInvestment * year,
+      });
+    }
+
+    // return annualData;
+    // this.data.emit(this.investmentData);
+    this.data = annualData;
+
+  }
+
   counter(i: number) {
     return new Array(i);
 }
